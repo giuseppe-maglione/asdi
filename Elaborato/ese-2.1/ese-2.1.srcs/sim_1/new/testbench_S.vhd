52e1b828719678f82e5b40e2644f96c9eb807faf
Giuseppe Maglione <148074888+giuseppe-maglione@users.noreply.github.com>
2024-11-27 21:53:51 +0100

    Add files via upload

---
+++ Elaborato/ese-2.1/ese-2.1.srcs/sim_1/new/testbench_S.vhd
@@ -0,0 +1,66 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity tb_system_S is
+end tb_system_S;
+
+architecture Behavioral of tb_system_S is
+
+    -- dichiarazione del sistema S
+    component system_S
+        Port (
+            clk : in std_logic;
+            addr : in std_logic_vector(3 downto 0);
+            enable : in std_logic;
+            data_out : out std_logic_vector(3 downto 0)
+        );
+    end component;
+
+    signal clk : std_logic := '0';
+    signal addr : std_logic_vector(3 downto 0);
+    signal enable : std_logic;
+    signal data_out : std_logic_vector(3 downto 0);
+
+    -- Clock period definition
+    constant clk_period : time := 10 ns;
+
+begin
+
+    -- instanziazione del sistema S
+    uut: system_S
+        port map ( clk => clk, addr => addr, enable => enable, data_out => data_out );
+
+    -- generazione del clock
+    clk_process: process
+    begin
+        while true loop
+            clk <= '0';
+            wait for clk_period / 2;
+            clk <= '1';
+            wait for clk_period / 2;
+        end loop;
+    end process;
+
+    process
+    begin
+
+        enable <= '0';
+        addr <= (others => '0');
+        wait for clk_period;
+
+        enable <= '1';
+        wait for clk_period;
+
+        for i in 0 to 15 loop
+            addr <= std_logic_vector(to_unsigned(i, 4));        -- testa il sistema per gli indirizzi possibili
+            wait for clk_period;
+        end loop;
+
+        enable <= '0';
+        wait for clk_period;
+
+        wait;
+    end process;
+
+end Behavioral;
---
+++ Elaborato/ese-2.1/ese-2.1.srcs/sources_1/new/system_S.vhd
@@ -0,0 +1,28 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity system_S is
+    Port ( 
+            clk: in std_logic;
+            addr: in std_logic_vector(3 downto 0);      -- indirizzo da cui leggere nella rom
+            enable: in std_logic;       -- segnale di abilitazione
+            data_out: out std_logic_vector(3 downto 0)      -- uscita elaborata dalla macchina M
+    );
+end system_S;
+
+architecture Behavioral of system_S is
+
+signal rom_data: std_logic_vector(7 downto 0);      -- segnale di appoggio per il dato prelevato dalla rom
+
+begin
+
+-- istanziazione rom
+rom_16x8: entity work.rom_generic
+    generic map ( 16, 8 , 4)
+    port map ( clk => clk, read => enable, addr => addr, data_out => rom_data );
+    
+-- istanziazione machine M
+machine_M: entity work.machine_M
+    port map ( data_in => rom_data, data_out => data_out );
+
+end Behavioral;
